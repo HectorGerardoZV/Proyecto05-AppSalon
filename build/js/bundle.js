ab1161f0434a41a8aa942a012ce4a763d600b958
TorroSoft <torrozapata@gmail.com>
2021-07-11 02:40:49 -0700

    Adding file services.json

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,3 +1,37 @@
-console.log(1);
+let section = 1;
+let content;
+
+document.addEventListener("DOMContentLoaded", () => {
+    startApp();
+});
+
+function startApp() {
+    events();
+
+}
+
+function events() {
+    const tabs = document.querySelectorAll(".tabs button");
+    tabs.forEach(tab => {
+        tab.addEventListener("click", e => {
+            insertSection(e);
+        })
+    });
+}
+
+
+function insertSection(e) {
+    const newSection = parseInt(e.target.dataset.section);
+    if (newSection == 1) {
+        console.log("Estamos en la section 1");
+    }
+    if (newSection == 2) {
+        console.log("Estamos en la section 2");
+    }
+    if (newSection == 3) {
+        console.log("Estamos en la section 3");
+    }
+
+}
 
 
